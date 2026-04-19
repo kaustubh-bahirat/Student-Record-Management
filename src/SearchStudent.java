6228a391381e0753153db298609cc9edc562f34a
karan-bahirat <karanbahirat1111@gmail.com>
2026-04-19 23:54:02 +0530

    Update SearchStudent.java

--- src/SearchStudent.java
+++ src/SearchStudent.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 
 public class SearchStudent {
+
     public static void search(HashTable ht) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter Roll No: ");
+        
+        System.out.println("\n========== SEARCH STUDENT ==========");
+        System.out.print("Enter Roll Number: ");
         int roll = sc.nextInt();
 
+        
         Student s = ht.search(roll);
 
+
+        System.out.println("\n----------------------------------");
+
         if (s != null) {
-            System.out.println("Found: " + s.rollNo + " " + s.name + " " + s.department + " " + s.marks);
+            System.out.println("        STUDENT FOUND");
+            System.out.println("----------------------------------");
+            System.out.println("Roll No     : " + s.rollNo);
+            System.out.println("Name        : " + s.name);
+            System.out.println("Department  : " + s.department);
+            System.out.println("Marks       : " + s.marks);
         } else {
-            System.out.println("Student not found!");
+            System.out.println("Student not found in records.");
         }
+
+        System.out.println("----------------------------------");
     }
 }
